42e05af70d35ce559bb55e2a08a46253af3a41d8
Efen Stakes <efenstakes101@gmail.com>
2023-09-09 19:15:20 +0300

    add logic to detect when tabs change and send message to popup

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,34 @@
 chrome.runtime.onInstalled.addListener(init);
 
+chrome.runtime.onMessage.addListener((message, sender, sendMessage)=> {
+
+    console.log('====================================');
+    console.log(`Service Worker Got message ${message}`);
+    console.log('====================================');
+})
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab)=> {
+
+    if( !tab.url ) {
+
+        console.log('====================================');
+        console.log("no tab url here, tab info is ", tab);
+        console.log('====================================');
+
+        return
+    }
+
+    // get url
+    const url = tab?.url
+
+    // get info about the site
+    
+    // send info to ui
+    chrome.runtime.sendMessage({ site: url, data: "Nothing Mate", })
+})
+
 function init() {}
+
+const getSitePhishingInfo = (site)=> {
+
+}
