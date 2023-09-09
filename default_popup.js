f24394602e9815ffc7cd0f733bada3bc9bd92179
Efen Stakes <efenstakes101@gmail.com>
2023-09-09 19:13:49 +0300

    add base popup script

--- default_popup.js
+++ default_popup.js
@@ -2,4 +2,29 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 
+    const btn = document.getElementsByClassName("testb")[0]
+
+    btn.addEventListener('click', ()=> {
+
+        console.log('====================================');
+        console.log("button clicked");
+        console.log('====================================');
+
+        chrome.runtime.sendMessage("HolaServiceWorker")
+    })
+
 }
+
+
+// listen for service worker messages
+chrome.runtime.onMessage.addListener((message, sender, sendMessage)=> {
+
+    console.log('====================================');
+    console.log("UI Got message", message);
+    console.log('====================================');
+
+    console.log('====================================');
+    console.log("UI Got sender ", sender);
+    console.log('====================================');
+})
+
